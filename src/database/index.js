b8bbec304010aadf326d3276f243f22343f51b6f
Saulo Henrique <saulohjr@hotmail.com>
2023-04-24 13:04:59 -0300

    client

--- src/database/index.js
+++ src/database/index.js
@@ -4,9 +4,13 @@ const connection = new Sequelize(dbConfig);
 
 const Perfil = require('../models/Perfil');
 const Usuario = require('../models/Usuario');
+const Cliente = require('../models/Cliente');
+const Endereco = require('../models/Enderecos');
 
 Perfil.init(connection);
 Usuario.init(connection);
+Cliente.init(connection);
+Endereco.init(connection);
 
 module.exports = connection;
 
